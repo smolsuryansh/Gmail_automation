2ceb2aa66520b2727c2feb158cb52820f0bdd4de
vedant Rajput <rajputvedant213@gmail.com>
2024-02-03 13:38:08 +0530

    add facts

--- src/main/java/org/hammy/App.java
+++ src/main/java/org/hammy/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        // mai chutiya hu - vedant
 
         System.out.println("Preparing to send message");
         System.out.println("Type in your message: ");
